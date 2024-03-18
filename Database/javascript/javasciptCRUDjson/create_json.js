4861f342eaf99ab263e21b14d92fbc971e0826f3
JungYoungSick <junfas1216@gmail.com>
2024-03-18 15:02:00 +0900

    update 파일 주석 처리 및 코드 내보내기 추가

--- Database/javascript/javasciptCRUDjson/create_json.js
+++ Database/javascript/javasciptCRUDjson/create_json.js
@@ -5,10 +5,11 @@ const data = {
   name: 'jung',
   age: 31,
 }
-function createJson(data) {
+export function createJson(data) {
   const jsonData = JSON.stringify(data, null, 4);
 
   const fs = require('fs');
+  // 파일 불러오기 형식을 사용.(파일시스템)
   fs.writeFile(filePath, jsonData, (err) => {
     if (err) {
       console.error(err);
--- Database/javascript/javasciptCRUDjson/update_json.js
+++ Database/javascript/javasciptCRUDjson/update_json.js
@@ -1,11 +1,15 @@
+// ! JSON 데이터를 업데이트 하는 함수
 export function updateJson(updateFunc) {
   try {
+    // JSON 파일 읽기(readFileSync를 이용한 파일 읽기)
     const jsonData = require('fs').readFileSync(filePath, 'utf-8');
     // JSON 데이터 파싱
     const data = JSON.parse(jsonData);
 
+    // 매개변수에 파싱된 데이터 읽기.(업데이트 파일 실행)
     updateFunc(data)
 
+    // JSON 파일 쓰기
     const updatedData = JSON.stringify(data, null, 4);
     require('fs').writeFileSync(filePath, updatedData)
   } catch (err) {
@@ -18,7 +22,8 @@ export function updateJson(updateFunc) {
   }
 }
 
-function updateAge(data) {
+// ! 함수에서 값을 찾아 값 데이터 업데이트
+export function updateAge(data) {
   const index = data.findIndex((item) => item.name === 'jung');
   if (index !== -1) {
     data[index].age = 31;
