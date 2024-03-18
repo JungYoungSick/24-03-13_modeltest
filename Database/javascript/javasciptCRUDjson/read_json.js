7081bef15388578f17b9d66e604d92c5095aad95
JungYoungSick <junfas1216@gmail.com>
2024-03-18 14:50:30 +0900

    read 파일 주석 처리

--- Database/javascript/javasciptCRUDjson/read_json.js
+++ Database/javascript/javasciptCRUDjson/read_json.js
@@ -3,9 +3,12 @@ export function readJson() {
   try {
     // JSON 파일 읽기
     const jsonData = require('fs').readFileSync(filePath, 'utf-8')
+    // JSON 데이터 파싱
     const data = JSON.parse(jsonData);
+    // 파싱 데이터 반환(반환을 통한 파일 출력)
     return data;
   } catch (err) {
+    // 값이 없을 때의 null값을 반환
     if (err.code === 'ENOENT') {
       return null;
     } else {
